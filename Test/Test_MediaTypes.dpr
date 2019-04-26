8284348d124f5e6b244cf3a6783240f8d08cd68e
silvioprog <silvioprog@gmail.com>
2019-04-26 00:28:35 -0300

    Added a tiny mime.types file and new tests for the media-types feature.

--- Test/Test_MediaTypes.dpr
+++ Test/Test_MediaTypes.dpr
@@ -29,11 +29,22 @@ program Test_MediaTypes;
 {$I Tests.inc}
 
 uses
+  RTLConsts,
   SysUtils,
+  Classes,
   BrookExtra,
   BrookLibraryLoader,
+  BrookReader,
   BrookMediaTypes,
   Test;
+
+const
+  MIME_TYPES_FILE = '../Examples/Common/mime.types';
+
+var
+  MIMEFileName: TFileName = Concat(
+{$IFDEF UNIX}'/etc/'{$ELSE}ExtractFilePath(ParamStr(0)){$ENDIF},
+    BROOK_MIME_FILE);
 
 type
 
@@ -392,6 +403,359 @@ begin
   end
 end;
 
+procedure DoMediaTypesParserCreateParamIsNilReader(const AArgs: array of const);
+begin
+  TBrookMediaTypesParser.Create(nil, TBrookMediaTypes(AArgs[0].VObject));
+end;
+
+procedure DoMediaTypesParserCreateParamIsNilTypes(const AArgs: array of const);
+begin
+  TBrookMediaTypesParser.Create(TBrookTextReader(AArgs[1].VObject), nil);
+end;
+
+procedure Test_MediaTypesParserCreate;
+var
+  R: TBrookTextReader;
+  T: TBrookMediaTypes;
+  P: TBrookMediaTypesParser;
+begin
+  R := TBrookStringReader.Create('');
+  T := TFakeMediaTypes.Create;
+  P := TBrookMediaTypesParser.Create(R, T);
+  try
+    Assert(Assigned(P.Reader) and (P.Reader = R));
+    Assert(Assigned(P.Types) and (P.Types = T));
+
+    AssertExcept(DoMediaTypesParserCreateParamIsNilReader,
+      EArgumentNilException, Format(SParamIsNil, ['AReader']), [T]);
+    AssertExcept(DoMediaTypesParserCreateParamIsNilTypes,
+      EArgumentNilException, Format(SParamIsNil, ['ATypes']), [R]);
+  finally
+    T.Free;
+    R.Free;
+    P.Free;
+  end;
+end;
+
+procedure DoMediaTypesParserParseInvalidMediaType(const AArgs: array of const);
+begin
+  TBrookMediaTypesParser(AArgs[0].VObject).Parse;
+end;
+
+procedure DoMediaTypesParserParseInvalidMediaExt(const AArgs: array of const);
+begin
+  TBrookMediaTypesParser(AArgs[0].VObject).Parse;
+end;
+
+procedure Test_MediaTypesParserParse;
+var
+  R: TBrookTextReader;
+  T: TBrookMediaTypes;
+  P: TBrookMediaTypesParser;
+begin
+  R := TBrookStringReader.Create(Concat(
+    'text/plain', #9,'txt', sLineBreak,
+    'text/html', #9,'html htm xhtml', sLineBreak,
+    'application/json', #9,'json', sLineBreak,
+    '#application/docbook+xml', #9,'docbook dbk', sLineBreak,
+    'application/pdf', #9,'pdf', sLineBreak,
+    'foo bar', sLineBreak,
+    'foo=bar'
+  ));
+  T := TFakeMediaTypes.Create;
+  P := TBrookMediaTypesParser.Create(R, T);
+  try
+    Assert(P.Types.Count = 0);
+    P.Parse;
+    Assert(P.Types.Count = 6);
+    Assert(P.Types.Find('.txt') = 'text/plain');
+    Assert(P.Types.Find('.html') = 'text/html');
+    Assert(P.Types.Find('.htm') = 'text/html');
+    Assert(P.Types.Find('.xhtml') = 'text/html');
+    Assert(P.Types.Find('.json') = 'application/json');
+    Assert(P.Types.Find('.docbook') = P.Types.DefaultType);
+    Assert(P.Types.Find('.pdf') = 'application/pdf');
+  finally
+    T.Free;
+    R.Free;
+    P.Free;
+  end;
+
+  R := TBrookStringReader.Create(Concat(
+    'text/plain', #9, 'txt', sLineBreak,
+    'application/json', #9, 'json', sLineBreak,
+    'foo', #9, 'bar'
+  ));
+  T := TFakeMediaTypes.Create;
+  P := TBrookMediaTypesParser.Create(R, T);
+  try
+    Assert(P.Types.Count = 0);
+    AssertExcept(DoMediaTypesParserParseInvalidMediaType,
+      EBrookMediaTypes, Format(SBrookInvalidMediaType, ['foo']), [P]);
+    Assert(P.Types.Count = 2);
+  finally
+    T.Free;
+    R.Free;
+    P.Free;
+  end;
+
+  R := TBrookStringReader.Create(Concat(
+    'text/plain', #9, 'txt', sLineBreak,
+    'application/json', #9, 'json', sLineBreak,
+    'foo/bar', #9, '.'
+  ));
+  T := TFakeMediaTypes.Create;
+  P := TBrookMediaTypesParser.Create(R, T);
+  try
+    Assert(P.Types.Count = 0);
+    AssertExcept(DoMediaTypesParserParseInvalidMediaExt,
+      EBrookMediaTypes, Format(SBrookInvalidMediaExt, ['.']), [P]);
+    Assert(P.Types.Count = 2);
+  finally
+    T.Free;
+    R.Free;
+    P.Free;
+  end;
+end;
+
+procedure Test_MediaTypesParserReader;
+var
+  R: TBrookTextReader;
+  T: TBrookMediaTypes;
+  P: TBrookMediaTypesParser;
+begin
+  R := TBrookStringReader.Create('');
+  T := TFakeMediaTypes.Create;
+  P := TBrookMediaTypesParser.Create(R, T);
+  try
+    Assert(Assigned(P.Reader) and (P.Reader = R));
+  finally
+    T.Free;
+    R.Free;
+    P.Free;
+  end;
+end;
+
+procedure Test_MediaTypesParserTypes;
+var
+  R: TBrookTextReader;
+  T: TBrookMediaTypes;
+  P: TBrookMediaTypesParser;
+begin
+  R := TBrookStringReader.Create('');
+  T := TFakeMediaTypes.Create;
+  P := TBrookMediaTypesParser.Create(R, T);
+  try
+    Assert(Assigned(P.Types) and (P.Types = T));
+  finally
+    T.Free;
+    R.Free;
+    P.Free;
+  end;
+end;
+
+procedure DoMediaTypesParserNginxParseInvalidMediaType(const AArgs: array of const);
+begin
+  TBrookMediaTypesParserNginx(AArgs[0].VObject).Parse;
+end;
+
+procedure DoMediaTypesParserNginxParseInvalidMediaExt(const AArgs: array of const);
+begin
+  TBrookMediaTypesParserNginx(AArgs[0].VObject).Parse;
+end;
+
+procedure Test_MediaTypesParserNginxParse;
+var
+  R: TBrookTextReader;
+  T: TBrookMediaTypes;
+  P: TBrookMediaTypesParser;
+begin
+  R := TBrookStringReader.Create(Concat(
+     sLineBreak,
+     'types {', sLineBreak,
+     '    text/html                                        html htm shtml;', sLineBreak,
+     sLineBreak,
+     '    text/css                                         css;', sLineBreak,
+     '#    text/xml                                         xml;', sLineBreak,
+     '    application/javascript                           js;', sLineBreak,
+     '#    application/atom+xml                             atom;', sLineBreak,
+     '    application/rss+xml                              rss;', sLineBreak,
+     '}', sLineBreak
+  ));
+  T := TFakeMediaTypes.Create;
+  P := TBrookMediaTypesParserNginx.Create(R, T);
+  try
+    Assert(P.Types.Count = 0);
+    P.Parse;
+    Assert(P.Types.Count = 6);
+    Assert(P.Types.Find('.html') = 'text/html');
+    Assert(P.Types.Find('.htm') = 'text/html');
+    Assert(P.Types.Find('.shtml') = 'text/html');
+    Assert(P.Types.Find('.js') = 'application/javascript');
+    Assert(P.Types.Find('.xml') = P.Types.DefaultType);
+    Assert(P.Types.Find('.rss') = 'application/rss+xml');
+  finally
+    T.Free;
+    R.Free;
+    P.Free;
+  end;
+
+  R := TBrookStringReader.Create(Concat(
+    'types {', sLineBreak,
+    '    text/html html;', sLineBreak,
+    '    text/css css;', sLineBreak,
+    '    foo bar;', sLineBreak,
+    '}'
+  ));
+  T := TFakeMediaTypes.Create;
+  P := TBrookMediaTypesParserNginx.Create(R, T);
+  try
+    Assert(P.Types.Count = 0);
+    AssertExcept(DoMediaTypesParserNginxParseInvalidMediaType,
+      EBrookMediaTypes, Format(SBrookInvalidMediaType, ['foo']), [P]);
+    Assert(P.Types.Count = 2);
+  finally
+    T.Free;
+    R.Free;
+    P.Free;
+  end;
+
+  R := TBrookStringReader.Create(Concat(
+    'types {', sLineBreak,
+    '    text/html html;', sLineBreak,
+    '    text/css css;', sLineBreak,
+    '    foo .;', sLineBreak,
+    '}'
+  ));
+  T := TFakeMediaTypes.Create;
+  P := TBrookMediaTypesParserNginx.Create(R, T);
+  try
+    Assert(P.Types.Count = 0);
+    AssertExcept(DoMediaTypesParserNginxParseInvalidMediaExt,
+      EBrookMediaTypes, Format(SBrookInvalidMediaExt, ['.']), [P]);
+    Assert(P.Types.Count = 2);
+  finally
+    T.Free;
+    R.Free;
+    P.Free;
+  end;
+end;
+
+procedure DoMediaTypesPathCreateFOpenError(const AArgs: array of const);
+begin
+  TBrookMediaTypesPath.Create(AArgs[0].VPChar);
+end;
+
+procedure Test_MediaTypesPathCreate;
+var
+  MT: TBrookMediaTypesPath;
+begin
+  MT := TBrookMediaTypesPath.Create(MIME_TYPES_FILE);
+  try
+    Assert(MT.FileName = MIME_TYPES_FILE);
+    Assert(Assigned(MT.Reader));
+    Assert(Assigned(MT.Parser));
+  finally
+    MT.Free;
+  end;
+
+  MT := TBrookMediaTypesPath.Create;
+  try
+    Assert(MT.FileName = MIMEFileName);
+    Assert(Assigned(MT.Reader));
+    Assert(Assigned(MT.Parser));
+  finally
+    MT.Free;
+  end;
+
+  AssertExcept(DoMediaTypesPathCreateFOpenError, EFOpenError,
+    Format(SFOpenError, ['']), [PChar('')]);
+  DeleteFile('foo');
+  AssertExcept(DoMediaTypesPathCreateFOpenError, EFOpenError,
+    Format(SFOpenError, ['foo']), [PChar('foo')]);
+end;
+
+procedure Test_MediaTypesPathGetDescription;
+begin
+  Assert(TBrookMediaTypesPath.GetDescription = 'Default');
+end;
+
+procedure Test_MediaTypesPathGetFileName;
+begin
+  Assert(TBrookMediaTypesPath.GetFileName = MIMEFileName);
+end;
+
+procedure Test_MediaTypesPathPrepare;
+var
+  MT: TBrookMediaTypesPath;
+begin
+  MT := TBrookMediaTypesPath.Create(MIME_TYPES_FILE);
+  try
+    Assert(not MT.Prepared);
+    MT.Prepare;
+    Assert(MT.Prepared);
+    Assert(MT.Find('.txt') = 'text/plain');
+    Assert(MT.Find('.html') = 'text/html');
+    Assert(MT.Find('.json') = 'application/json');
+    Assert(MT.Find('.pdf') = 'application/pdf');
+    Assert(MT.Find('.xxx') = MT.DefaultType);
+  finally
+    MT.Free;
+  end;
+end;
+
+procedure Test_MediaTypesPathClear;
+var
+  MT: TBrookMediaTypesPath;
+begin
+  MT := TBrookMediaTypesPath.Create(MIME_TYPES_FILE);
+  try
+    Assert(MT.Count = 0);
+    MT.Prepare;
+    Assert(MT.Count > 0);
+    MT.Clear;
+    Assert(MT.Count = 0);
+  finally
+    MT.Free;
+  end;
+end;
+
+procedure Test_MediaTypesPathReader;
+var
+  MT: TBrookMediaTypesPath;
+begin
+  MT := TBrookMediaTypesPath.Create(MIME_TYPES_FILE);
+  try
+    Assert(Assigned(MT.Reader));
+  finally
+    MT.Free;
+  end;
+end;
+
+procedure Test_MediaTypesPathParser;
+var
+  MT: TBrookMediaTypesPath;
+begin
+  MT := TBrookMediaTypesPath.Create(MIME_TYPES_FILE);
+  try
+    Assert(Assigned(MT.Parser));
+  finally
+    MT.Free;
+  end;
+end;
+
+procedure Test_MediaTypesPathFileName;
+var
+  MT: TBrookMediaTypesPath;
+begin
+  MT := TBrookMediaTypesPath.Create(MIMEFileName);
+  try
+    Assert(MT.FileName = MIMEFileName);
+  finally
+    MT.Free;
+  end;
+end;
+
 begin
   TBrookLibraryLoader.Load;
   Test_MediaTypesCreate;
@@ -411,4 +775,18 @@ begin
   Test_MediaTypesClear;
   Test_MediaTypesDefaultType;
   Test_MediaTypesPrepared;
+  Test_MediaTypesParserCreate;
+  Test_MediaTypesParserParse;
+  Test_MediaTypesParserReader;
+  Test_MediaTypesParserTypes;
+  Test_MediaTypesParserNginxParse;
+  Test_MediaTypesPathCreate;
+  // Test_MediaTypesPathDestroy - not required
+  Test_MediaTypesPathGetDescription;
+  Test_MediaTypesPathGetFileName;
+  Test_MediaTypesPathPrepare;
+  Test_MediaTypesPathClear;
+  Test_MediaTypesPathReader;
+  Test_MediaTypesPathParser;
+  Test_MediaTypesPathFileName;
 end.
